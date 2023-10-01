68a8e36836ff6aae47897444426f549484d448f2
Natthanan Gumyan <natthanan.gy@proxumer.com>
2023-10-01 13:30:48 +0700

    Initial Commit

---
+++ TopValue/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TopValue
+//
+//  Created by Natthanan Gumyan on 1/10/2566 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ TopValueUITests/TopValueUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  TopValueUITestsLaunchTests.swift
+//  TopValueUITests
+//
+//  Created by Natthanan Gumyan on 1/10/2566 BE.
+//
+
+import XCTest
+
+final class TopValueUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
